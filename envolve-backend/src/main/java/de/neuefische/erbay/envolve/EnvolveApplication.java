01f314aca5eb0fcc6db02abd7a64e76db8aaae81
Civan Erbay <Civan.Erbay@web.de>
2020-07-09 12:35:36 +0200

    Init Project

---
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/EnvolveApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.erbay.envolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvolveApplication.class, args);
+	}
+
+}
---
+++ envolve-backend/src/test/java/de/neuefische/erbay/envolve/EnvolveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.erbay.envolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
