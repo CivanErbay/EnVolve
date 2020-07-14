f0c4b2aa1ee3b0f122a56d52ca5dc178077b3024
Civan Erbay <Civan.Erbay@web.de>
2020-07-14 11:08:01 +0200

    Add Register Form with handleChange and State

--- envolve-frontend/src/pages/Register.jsx
+++ envolve-frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {Box} from "@material-ui/core";
-import React from "react";
+import React, {useState} from "react";
 import TextField from "@material-ui/core/TextField";
 import MyButton from "../components/MyButton";
 import makeStyles from "@material-ui/core/styles/makeStyles";
@@ -25,17 +25,33 @@ const useStyles = makeStyles((theme) => ({
 export default function Register() {
 
     const classes = useStyles();
+    const [registerState, setRegisterState] = useState({
+        username: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+       })
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setRegisterState({
+            ...registerState,
+            [name]: value
+        });
+    }
 
     return (
-        <Box mt={8} className={classes.outer}>
+        <Box mt={12} className={classes.outer}>
             <h2>Register</h2>
             <Box className={classes.inner}>
+                <TextField style={{width: "320px"}} id="standard-basic" onChange={handleChange} name="username" label="Username"/>
                 <Box>
-                    <TextField style={{width: "150px", margin: "10px"}} id="standard-basic" label="Name"/>
-                    <TextField style={{width: "150px", margin: "10px"}} id="standard-basic" label="Lastname"/>
+                    <TextField style={{width: "150px", margin: "10px"}} onChange={handleChange} name="firstname" id="standard-basic" label="Name"/>
+                    <TextField style={{width: "150px", margin: "10px"}} onChange={handleChange} name="lastname" id="standard-basic" label="Lastname"/>
                 </Box>
-                <TextField style={{width: "320px"}} id="standard-basic" label="Email Address"/>
-                <TextField style={{width: "320px", margin: "10px"}} id="standard-basic" label="Password"/>
+                <TextField style={{width: "320px"}} id="standard-basic" name="email" onChange={handleChange} label="Email Address"/>
+                <TextField style={{width: "320px", margin: "10px"}} id="standard-basic" onChange={handleChange} name="password" label="Password"/>
                 <TextField style={{width: "320px"}} id="standard-basic" label="Confirm Password"/>
                 <Box mt={5}>
                     <MyButton content={"Submit"}/>
