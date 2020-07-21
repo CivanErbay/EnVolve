d45247b67519c7168aa2c665e56f4b37cb42f182
Civan Erbay <Civan.Erbay@web.de>
2020-07-21 16:36:38 +0200

    Rework about

--- envolve-frontend/src/components/About.jsx
+++ envolve-frontend/src/components/About.jsx
@@ -9,8 +9,8 @@ const useStyles = makeStyles((theme) => ({
     },
     headline: {
         fontFamily: "Modak",
-        fontSize: "2.5em",
-        padding: "1.5em 1.5em 0.5em 1.5em"
+        fontSize: "4.5em",
+        padding: "1em 1em 0.5em 1em"
     }
 }))
 
@@ -20,11 +20,12 @@ export const About = () => {
 
     return (
 
-        <Box className={classes.about}>
+        <Box pb={2} className={classes.about}>
             <Typography className={classes.headline}>About</Typography>
-            <img style={{height: "20vh"}} src="./images/twoPersons.png" alt=""/>
-            <Box px={5} py={4}>
-                <Typography style={{textAlign: "justify", textJustify: "inter-word"}}><b>EnVolve </b> is an app to <b>improve communication between students and their teachers </b>.
+
+            <Box px={3} py={4} mx={4} boxShadow={3} style={{maxWidth: "600px", margin: "0 auto", borderRadius: "15px", backgroundColor:"white"}}>
+                <img style={{height: "25vh"}} src="./images/twoPersons.png" alt=""/>
+                <Typography style={{textAlign: "justify", textJustify: "inter-word", padding: "1em", fontSize:"1.25em", lineHeight:"1.75"}}><span style={{fontFamily:"Modak", fontSize:"1.5em", lineHeight:".75"}}>EnVolve </span> is an app to <b>improve communication between students and their teachers </b>.
                     <br /> The approach is to
                 establish a <b>continuous student-side feedback loop</b> to allow students to share their interests, ideas,
                 wishes, criticisms and so on.
