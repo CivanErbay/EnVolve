1a60c54b208744192d52fd0191fe8b0cf68427c9
Civan Erbay <Civan.Erbay@web.de>
2020-07-21 17:15:18 +0200

    Fix About

--- envolve-frontend/src/components/About.jsx
+++ envolve-frontend/src/components/About.jsx
@@ -5,7 +5,11 @@ import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     about: {
-        backgroundColor: "#F7F7F7"
+        backgroundColor: "#F7F7F7",
+        display: "flex",
+        width: "100%",
+        flexDirection: "column",
+        alignItems: "center"
     },
     headline: {
         fontFamily: "Modak",
@@ -23,9 +27,9 @@ export const About = () => {
         <Box pb={2} className={classes.about}>
             <Typography className={classes.headline}>About</Typography>
 
-            <Box px={3} py={4} mx={4} boxShadow={3} style={{maxWidth: "600px", margin: "0 auto", borderRadius: "15px", backgroundColor:"white"}}>
+            <Box px={3} py={4} m={5} boxShadow={3} style={{maxWidth: "600px", backgroundColor: "#3AD19B"}}>
                 <img style={{height: "25vh"}} src="./images/twoPersons.png" alt=""/>
-                <Typography style={{textAlign: "justify", textJustify: "inter-word", padding: "1em", fontSize:"1.25em", lineHeight:"1.75"}}><span style={{fontFamily:"Modak", fontSize:"1.5em", lineHeight:".75"}}>EnVolve </span> is an app to <b>improve communication between students and their teachers </b>.
+                <Typography style={{ textAlign: "left", padding: "1em", fontSize:"1rem", lineHeight:"1.75"}}><span style={{fontFamily:"Modak", fontSize:"1.5em", lineHeight:".75"}}>EnVolve </span> is an app to <b>improve communication between students and their teachers </b>.
                     <br /> The approach is to
                 establish a <b>continuous student-side feedback loop</b> to allow students to share their interests, ideas,
                 wishes, criticisms and so on.
