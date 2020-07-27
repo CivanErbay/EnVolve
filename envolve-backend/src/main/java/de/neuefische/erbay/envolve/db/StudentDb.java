97e07c68317f4479fd34fbb8147643190d2d8023
Civan Erbay <Civan.Erbay@web.de>
2020-07-27 19:49:06 +0200

    Rework all the other DB's for Firebase DB

--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/db/StudentDb.java
+++
@@ -1,9 +0,0 @@
-package de.neuefische.erbay.envolve.db;
-
-import de.neuefische.erbay.envolve.model.Student;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface StudentDb extends PagingAndSortingRepository<Student, String> {
-}
-
-
