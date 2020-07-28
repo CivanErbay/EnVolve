4d74b125497f6f9e4c070e61582d96639f4eb7dc
Civan Erbay <Civan.Erbay@web.de>
2020-07-28 09:52:05 +0200

    Some fixes

--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/service/UserService.java
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/service/UserService.java
@@ -4,6 +4,7 @@ import de.neuefische.erbay.envolve.model.Teacher;
 import de.neuefische.erbay.envolve.model.dto.LoginDto;
 import de.neuefische.erbay.envolve.model.dto.TeacherRegisterDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,6 +15,7 @@ import java.util.concurrent.ExecutionException;
 public class UserService {
 
     private final TeacherDb teacherDb;
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 
     @Autowired
@@ -22,7 +24,15 @@ public class UserService {
     }
 
     public Teacher register (TeacherRegisterDto data) {
-      return teacherDb.register(data);
+        de.neuefische.erbay.envolve.model.Teacher teacher = new Teacher();
+        teacher.setUsername(data.getUsername());
+        String encodedPw = encoder.encode(data.getPassword());
+        teacher.setPassword(encodedPw);
+        teacher.setFirstname(data.getFirstname());
+        teacher.setLastname(data.getLastname());
+        teacher.setEmail(data.getEmail());
+
+      return teacherDb.register(teacher);
     }
 
     public String login(@RequestBody LoginDto data) throws ExecutionException, InterruptedException {
