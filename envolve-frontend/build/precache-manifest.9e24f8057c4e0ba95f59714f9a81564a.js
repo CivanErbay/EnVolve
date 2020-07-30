1e713917bb41db8a846805bd7e8a45ed9dec6d0a
Civan Erbay <Civan.Erbay@web.de>
2020-07-30 16:00:20 +0200

    Add minor things

---
+++ envolve-frontend/build/precache-manifest.9e24f8057c4e0ba95f59714f9a81564a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a7ddbf5aeaf7956a704e305aa9a531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd91114631536b68b54d",
+    "url": "/static/css/main.f83e80fa.chunk.css"
+  },
+  {
+    "revision": "f26c11a5f18c0de8b1bc",
+    "url": "/static/js/2.78cd3ff1.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.78cd3ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd91114631536b68b54d",
+    "url": "/static/js/main.44f5cc39.chunk.js"
+  },
+  {
+    "revision": "f40046aaf677781b949a",
+    "url": "/static/js/runtime-main.d59ab3f1.js"
+  }
+]);
