d10e02b31b77a58bb94bd057143b72913be47c8c
Civan Erbay <Civan.Erbay@web.de>
2020-08-03 18:28:23 +0200

    Change envConfig

--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/config/EnvConfig.java
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/config/EnvConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/*
 @Configuration
 public class EnvConfig {
     @Bean
@@ -17,8 +18,21 @@ public class EnvConfig {
             };
         };
         };
-    }
+    }*/
 
+@Configuration
+public class EnvConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://envolve-2d3d3.web.app/","https://envolve-feedback.herokuapp.com/")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
 
 
 
