dc7cfa1d991b45be9f46ccf0bd0f76061216f413
Civan Erbay <Civan.Erbay@web.de>
2020-08-03 12:37:50 +0200

    Add Cors Config Method

---
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/config/EnvConfig.java
@@ -0,0 +1,25 @@
+package de.neuefische.erbay.envolve.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class EnvConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://envolve-2d3d3.web.app/","https://envolve-2d3d3.firebaseapp.com/")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");;
+            };
+        };
+        };
+    }
+
+
+
+
