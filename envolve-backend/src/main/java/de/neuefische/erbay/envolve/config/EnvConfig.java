3db12ad2e83ab9146127902767556723b346b6ee
Civan Erbay <Civan.Erbay@web.de>
2020-08-03 16:08:21 +0200

    rework deploy yml

--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/config/EnvConfig.java
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/config/EnvConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class EnvConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://envolve-2d3d3.web.app/","https://envolve-2d3d3.firebaseapp.com/")
