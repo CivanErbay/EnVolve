76dc7e224de534add854982aa4db27592e846c60
Civan Erbay <Civan.Erbay@web.de>
2020-08-03 16:13:54 +0200

    kickout mongoDb stuff in models

--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/model/NewSurvey.java
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/model/NewSurvey.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+/*
 import org.springframework.data.mongodb.core.mapping.Document;
+*/
 
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+/*
 @Document(collection = "newsurvey")
+*/
 public class NewSurvey {
 
     @Id
--- envolve-backend/src/main/java/de/neuefische/erbay/envolve/model/SurveyAnswer.java
+++ envolve-backend/src/main/java/de/neuefische/erbay/envolve/model/SurveyAnswer.java
@@ -4,15 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+/*
 import org.springframework.data.mongodb.core.mapping.Document;
+*/
 
-import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+/*
 @Document(collection = "surveyanswer" )
+*/
 public class SurveyAnswer {
 
     @Id
