a5422840f66f9f0f0ca25ad104dfdec75422132d
Civan Erbay <Civan.Erbay@web.de>
2020-08-05 12:47:18 +0200

    Display keywords on dashboard

--- envolve-frontend/src/components/Dashboard.jsx
+++ envolve-frontend/src/components/Dashboard.jsx
@@ -35,35 +35,40 @@ export const Dashboard = ({schoolClassId}) => {
     const [resultByWeek, setResultByWeek] = useState([])
     const [lastWeekResult, setLastWeekResult] = useState([])
     const [prevLastWeekResult, setPrevLastWeekResult] = useState([])
+    const [keywordList, setKeywordList] = useState([])
     const [data, setData] = useState({data1: undefined, data2: undefined, data3: undefined})
 
     useEffect(() => {
-        const data1 = [
+        {
+            const data1 =
+                keywordList.map((keyword, index) => {
+                    return {subject: keyword, A: lastWeekResult[index]}
+                })
 
-            {subject: 'Atmosphere', A: lastWeekResult[0], B: 1.1, fullMark: 5},
-            {subject: 'Participation', A: lastWeekResult[1], B: 1.3, fullMark: 5},
-            {subject: 'Concentration', A: lastWeekResult[2], B: 1.3, fullMark: 5},
-            {subject: 'Benefit', A: lastWeekResult[3], B: 1, fullMark: 5},
-            {subject: 'Motivation', A: lastWeekResult[4], B: 5, fullMark: 5},
+            const keywordListShortCapitalize = []
+            keywordList.map(keyword => {
+               let keywordShort = '';
+                for(let i = 0; i < 4; i++) {
+                  keywordShort += keyword[i].toUpperCase();
+                }
+                keywordListShortCapitalize.push(keywordShort)
+            });
 
-        ];
-        const data2 = [
-            {name: 'ATMO', lastWeek: prevLastWeekResult[0], currentWeek: lastWeekResult[0]},
-            {name: 'PART', lastWeek: prevLastWeekResult[1], currentWeek: lastWeekResult[1] },
-            {name: 'CONC', lastWeek: prevLastWeekResult[2], currentWeek: lastWeekResult[2]},
-            {name: 'BENE', lastWeek: prevLastWeekResult[3], currentWeek: lastWeekResult[3]},
-            {name: 'MOTI', lastWeek: prevLastWeekResult[4], currentWeek: lastWeekResult[4]},
 
-        ];
-        const data3 =
-            resultByWeek.map((singleWeekResult, index) => {
-                return {name: `W${index + 1}`, uv: singleWeekResult, pv: 1.3, amt: 2.0}
-            })
-        ;
-        setData({data1,data2,data3})
+            const data2 =
+                keywordListShortCapitalize.map((keyword, index) => {
+                  return {name: keyword, lastWeek: prevLastWeekResult[index], currentWeek: lastWeekResult[index] }
+                })
+
+            const data3 =
+                resultByWeek.map((singleWeekResult, index) => {
+                    return {name: `W${index + 1}`, uv: singleWeekResult, pv: 1.3, amt: 2.0}
+                })
+            ;
+            setData({data1, data2, data3})
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[prevLastWeekResult,lastWeekResult])
-
+    }, [keywordList])
 
 
     //FETCH ALL SURVEYANSWER (WHOLE OBJECT)
@@ -88,7 +93,18 @@ export const Dashboard = ({schoolClassId}) => {
                     return result.localDate.getTime() > new Date().getTime() - ((i + 1) * 7 * 24 * 60 * 60 * 1000) && result.localDate.getTime() < new Date().getTime() - (i * 7 * 24 * 60 * 60 * 1000)
                 }));
             }
-            // NEXT TASK - TAKE OUT KeyWord out of weekResults/allSurveyanswers and map the Data!
+
+            //Get Keywords out of Questions
+            const tempKeywordList = []
+            allSurveyAnswers.map(singleSurveyAnswer => {
+                    singleSurveyAnswer.questionList.map(singleQuestion => {
+                      tempKeywordList.push(singleQuestion.keyWord)
+                    })
+            })
+            setKeywordList(tempKeywordList)
+
+
+
             //First Table
             setLastWeekResult(lastWeekResponseCalculator(weekResults[0]));
 
@@ -116,7 +132,7 @@ export const Dashboard = ({schoolClassId}) => {
                 responses.push(week[i].questionList[j].response)
             }
         }
-        if(responses.length > 0) {
+        if (responses.length > 0) {
             const sum = responses.reduce((curr, acc) => {
                 return curr + acc
             })
@@ -128,7 +144,7 @@ export const Dashboard = ({schoolClassId}) => {
 
     const lastWeekResponseCalculator = (student) => {
 
-    /*    let tempResponses = Array(student[0].questionList.length).fill([])*/
+        /*    let tempResponses = Array(student[0].questionList.length).fill([])*/
         let tempResponses = [[], [], [], [], []]
         for (let j = 0; j < student.length; j++) {
             for (let i = 0; i < student[j].questionList.length; i++) {
@@ -138,8 +154,8 @@ export const Dashboard = ({schoolClassId}) => {
 
         let finalResponses = []
 
-        for (let v = 0; v < tempResponses.length; v++){
-           finalResponses.push(average(tempResponses[v]))
+        for (let v = 0; v < tempResponses.length; v++) {
+            finalResponses.push(average(tempResponses[v]))
         }
         return finalResponses
 
@@ -147,78 +163,79 @@ export const Dashboard = ({schoolClassId}) => {
 
     const average = (array) => {
         let sum = 0;
-        for(let p = 0; p < array.length; p++) {
+        for (let p = 0; p < array.length; p++) {
             sum += array[p];
         }
-        return sum/array.length
+        return sum / array.length
     }
-    console.log(data)
+
+
     return (
         <Box color={"secondary"} className={classes.stretch}>
 
             {data.data1 &&
-                <>
-            <Typography style={{fontSize: "2.5em", fontWeight: "bold", padding: "0.25em", textAlign: "left"}}>LAST
-                WEEK</Typography>
+            <>
+                <Typography style={{fontSize: "2.5em", fontWeight: "bold", padding: "0.25em", textAlign: "left"}}>LAST
+                    WEEK</Typography>
                 <Box mt={-3}>
-                <RadarChart cx={140} cy={140} outerRadius={60} width={300} height={250} data={data.data1}>
-                <PolarGrid/>
-                <PolarAngleAxis dataKey="subject"/>
-                <PolarRadiusAxis domain={[0, 5]} />
-                <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#D9EFF9" fillOpacity={0.6}/>
-                </RadarChart>
+                    <RadarChart cx={140} cy={140} outerRadius={60} width={300} height={250} data={data.data1}>
+                        <PolarGrid/>
+                        <PolarAngleAxis dataKey="subject"/>
+                        <PolarRadiusAxis domain={[0, 5]}/>
+                        <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#D9EFF9" fillOpacity={0.6}/>
+                    </RadarChart>
                 </Box>
 
 
                 <Box mt={4}>
-                <Typography style={{
-                fontSize: "2.5em",
-                fontWeight: "bold",
-                padding: "0.25em",
-                textAlign: "left"
-            }}>CHANGES</Typography>
-                <Box mt={2} ml={-6}>
-                <BarChart width={350} height={300} data={data.data2}
-                margin={{top: 5, right: 1, left: 1, bottom: 5}}>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="name"/>
-                <YAxis domain={[0, 5]}/>
-                <Legend/>
-                <Bar dataKey="lastWeek" fill="#39A4D1"/>
-                <Bar dataKey="currentWeek" fill="#82ca9d"/>
-                </BarChart>
-                </Box>
+                    <Typography style={{
+                        fontSize: "2.5em",
+                        fontWeight: "bold",
+                        padding: "0.25em",
+                        textAlign: "left"
+                    }}>CHANGES</Typography>
+                    <Box mt={2} ml={-6}>
+                        <BarChart width={350} height={300} data={data.data2}
+                                  margin={{top: 5, right: 1, left: 1, bottom: 5}}>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="name"/>
+                            <YAxis domain={[0, 5]}/>
+                            <Legend/>
+                            <Bar dataKey="lastWeek" fill="#39A4D1"/>
+                            <Bar dataKey="currentWeek" fill="#82ca9d"/>
+                        </BarChart>
+                    </Box>
                 </Box>
 
                 <Box mt={6}>
-                <Typography style={{
-                fontSize: "2.5em",
-                fontWeight: "bold",
-                padding: "0.25em",
-                textAlign: "left"
-            }}>OVERALL</Typography>
-                <Box mt={2} ml={-4}>
-                <AreaChart
-                width={350}
-                height={300}
-                data={data.data3}
-                margin={{top: 10, right: 30, left: 0, bottom: 0}}
-                >
-                <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="name"/>
-                <YAxis domain={[0, 5]}/>
+                    <Typography style={{
+                        fontSize: "2.5em",
+                        fontWeight: "bold",
+                        padding: "0.25em",
+                        textAlign: "left"
+                    }}>OVERALL</Typography>
+                    <Box mt={2} ml={-4}>
+                        <AreaChart
+                            width={350}
+                            height={300}
+                            data={data.data3}
+                            margin={{top: 10, right: 30, left: 0, bottom: 0}}
+                        >
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="name"/>
+                            <YAxis domain={[0, 5]}/>
 
-                <defs>
-                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                <stop stopColor="green" stopOpacity={1}/>
-                {/*offset={off}*/}
-                <stop stopColor="#D9EFF9" stopOpacity={1}/>
-                {/*offset={off}*/}
-                </linearGradient>
-                </defs>
-                <Area type="monotone" dataKey="uv" stroke="#000" fill="url(#splitColor)"/>
-                </AreaChart>
-                </Box>
+                            <defs>
+                                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                                    <stop stopColor="green" stopOpacity={1}/>
+                                    {/*offset={off}*/}
+                                    <stop stopColor="#D9EFF9" stopOpacity={1}/>
+                                    {/*offset={off}*/}
+                                </linearGradient>
+                            </defs>
+                            <Area type="monotone" dataKey="uv" stroke="#000" fill="url(#splitColor)"/>
+                        </AreaChart>
+                    </Box>
                 </Box>
             </>
             }
