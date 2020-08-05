885c2101709880415ed890fb2ec0aa252d3683bc
Civan Erbay <Civan.Erbay@web.de>
2020-08-05 16:17:21 +0200

    fix CorsConfgi

---
+++ envolve-frontend/build/precache-manifest.958af894629aba6a22487608a66772a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d268fb9ed94b5e425854a0565018f5a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6aadf68f552ebe462a2",
+    "url": "/static/css/main.f83e80fa.chunk.css"
+  },
+  {
+    "revision": "f739b265198a1b9e2fd6",
+    "url": "/static/js/2.1140ed04.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.1140ed04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6aadf68f552ebe462a2",
+    "url": "/static/js/main.7d920274.chunk.js"
+  },
+  {
+    "revision": "f40046aaf677781b949a",
+    "url": "/static/js/runtime-main.d59ab3f1.js"
+  }
+]);
