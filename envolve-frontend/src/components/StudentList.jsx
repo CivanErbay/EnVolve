d65fd559ad3a843ed72f6397af4f12eb1b47e346
Civan Erbay <Civan.Erbay@web.de>
2020-08-11 11:35:05 +0200

    Add Student Status Check in Frontend Dashboard Page

--- envolve-frontend/src/components/StudentList.jsx
+++ envolve-frontend/src/components/StudentList.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 import {getClassById} from "../utils/fetch-utils";
 import WhiteWrapper from "./wrapper/WhiteWrapper";
 import Typography from "@material-ui/core/Typography";
-
+import Grid from "@material-ui/core/Grid";
 
 
 export const StudentList = ({id}) => {
@@ -11,23 +11,52 @@ export const StudentList = ({id}) => {
     const [schoolClass, setSchoolClass] = useState(null)
 
 
-    useEffect(()=> {
+    useEffect(() => {
         getClassById(id).then(response => {
             setSchoolClass(response)
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+    }, [])
 
     return (
 
-        <Box>
 
-        {schoolClass &&  (    <WhiteWrapper style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        {schoolClass.classmembers.map((member) =>
-             <Typography style={{margin: "0 0.5em 0 0.5em"}} key={member.student}>{member.student} ({member.code})</Typography>)} </WhiteWrapper>)
-        }
+        <>
 
-        </Box>
+            {schoolClass && (<WhiteWrapper>
+                <Grid container spacing={1}  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%"
+                }}>
+                    {schoolClass.classmembers.map((member) =>
+                        <Box style={{
+                            margin: "1em",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            flexDirection: "column",
+                            border: "solid",
+                            borderWidth: "1px",
+                            padding: "1.1em",
+                            borderRadius: "10px"
+                        }}>
+                            <Grid item sm={3}>
+                                <Typography style={{textAlign: "left", fontSize: "1.3em", fontWeight: "700"}}
+                                            key={member.student}> {member.student}  </Typography>
+                            </Grid>
+                            <Grid item sm={3} >
+                                {member.activeStatus ?
+                                    <span><b> {member.code}</b></span> :
+                                    <img style={{height: "4vh", marginTop: "5px"}} src="/images/tick.svg"
+                                         alt=""/>}
+                            </Grid>
+                        </Box>
+                    )}     </Grid> </WhiteWrapper>)
+            }
+
+        </>
 
     )
 }
