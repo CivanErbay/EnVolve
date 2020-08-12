b9cfd10a6a4c4a246618d83c20b7cf9be7e68a1c
Civan Erbay <Civan.Erbay@web.de>
2020-08-12 12:20:33 +0200

    Add Overview Details about SurveyState

--- envolve-frontend/src/reducers/index.js
+++ envolve-frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import UserReducer from "./UserReducer";
 import {combineReducers} from "redux";
+import SchoolClassReducer from "./SchoolClassReducer";
 
- const allReducers = combineReducers({
-    loggedUser : UserReducer,
+const allReducers = combineReducers({
+    loggedUser: UserReducer,
+    schoolClassesData: SchoolClassReducer,
 })
 
 export default allReducers
