ac6203c606ba8ca3b15030412bb411726d3caa46
Civan Erbay <Civan.Erbay@web.de>
2020-08-12 12:23:36 +0200

    chunk changes

---
+++ envolve-frontend/build/precache-manifest.a11f60a401f349b18c2f3cf540398fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc5c58c555558b4421cdc3e9b80750f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "881caf8c2729f019a3e3",
+    "url": "/static/css/main.f83e80fa.chunk.css"
+  },
+  {
+    "revision": "62aa5c5e17922f297f51",
+    "url": "/static/js/2.1307caa1.chunk.js"
+  },
+  {
+    "revision": "d2c9eb7e98f0d312a7cc7aa81915c29a",
+    "url": "/static/js/2.1307caa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "881caf8c2729f019a3e3",
+    "url": "/static/js/main.3d1c1b29.chunk.js"
+  },
+  {
+    "revision": "f40046aaf677781b949a",
+    "url": "/static/js/runtime-main.d59ab3f1.js"
+  }
+]);
